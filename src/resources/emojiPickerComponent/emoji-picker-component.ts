73f9905a1f398031a8df840e383ec6c2f5d9618b
rhodeen <rhodeendavies@gmail.com>
2023-02-11 21:16:50 +0200

    fixed emoji picker popup

--- src/resources/emojiPickerComponent/emoji-picker-component.ts
+++ src/resources/emojiPickerComponent/emoji-picker-component.ts
@@ -9,18 +9,34 @@ export class EmojiPickerComponent {
 	@bindable({ defaultBindingMode: bindingMode.twoWay }) emoji = null;
 	@bindable onChange;
 	id: string;
+	pickerId: string;
 	pickerOpen: boolean = false;
 	blurring: boolean = false;
 	clickSub: Subscription;
 
 	constructor(private ea: EventAggregator) {
 		this.id = ComponentHelper.CreateId("emojiPicker");
+		this.pickerId = ComponentHelper.CreateId("emojiPickerBox");
 	}
 
 	attached() {
 		this.clickSub = this.ea.subscribe(Events.PickerClicked, (clickedId: string) => {
 			if (clickedId == this.id) {
 				this.pickerOpen = !this.pickerOpen;
+
+				setTimeout(() => {
+					const offset = $(`#${this.id}`).offset();
+	
+					let top = offset.top - $(window).scrollTop();
+					if (top + $(`#${this.pickerId}`).height() > $(window).height()) {
+						top = offset.top - $(window).scrollTop() - $(`#${this.pickerId}`).height() + $(`#${this.id}`).height()
+					}
+	
+					$(`#${this.pickerId}`).css({
+						"top": top,
+						"left": offset.left + $(`#${this.id}`).width()
+					});
+				});
 			} else {
 				this.pickerOpen = false;
 			}
