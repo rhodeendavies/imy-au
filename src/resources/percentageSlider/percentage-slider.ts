49917b106d10a485b0437d6f318419f5ecafe33c
rhodeen <rhodeendavies@gmail.com>
2023-02-18 22:56:54 +0200

    fixed percentage cache issue

--- src/resources/percentageSlider/percentage-slider.ts
+++ src/resources/percentageSlider/percentage-slider.ts
@@ -5,12 +5,20 @@ import { bindable } from "aurelia-templating";
 @autoinject
 export class PercentageSlider {
 
-	@bindable({ defaultBindingMode: bindingMode.twoWay }) percentage: number;
+	@bindable({ defaultBindingMode: bindingMode.twoWay }) percentage: number = null;
 	@bindable showCircle: boolean = true;
 	@bindable showValues: boolean = true;
 	@bindable lowText: string = "Not great";
 	@bindable highText: string = "Excellent";
 	@bindable onChange;
+	loaded: boolean = false;
+
+	attached() {
+		this.loaded = false;
+		setTimeout(() => {
+			this.loaded = true;
+		}, 10);
+	}
 
 	onChangeTrigger() {
 		if (this.onChange != null) {
